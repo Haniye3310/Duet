303ca8fd81a39f6be8bf6c4dbb62c28a86a4a362
Haniye Esmaeili <esmaeilihaniye@yahoo.com>
2021-10-18 16:18:55 +0330

    Duet Project , First Commit

---
+++ Assets/Scripts/Enviroment.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enviroment : MonoBehaviour
+{
+    [SerializeField] private GameObject _repeatableObject;
+    [SerializeField]Player _player;
+    public static bool _reseting = false;
+
+
+    void Update()
+    {
+        if (!_reseting) 
+        {
+            if ((_repeatableObject.transform.position.y - _player.transform.position.y) < 0.1f)
+            {
+                _repeatableObject = Instantiate(_repeatableObject, new Vector2(_repeatableObject.transform.position.x, _repeatableObject.GetComponent<SpriteRenderer>().bounds.size.y + _repeatableObject.transform.position.y), Quaternion.identity, this.transform);
+            }
+            this.transform.position = new Vector3(this.transform.position.x, Mathf.Lerp(this.transform.position.y, this.transform.position.y - 1.5f, Time.deltaTime), this.transform.position.z);
+        }
+        if (_reseting) 
+        {
+            this.transform.position = new Vector3(this.transform.position.x, Mathf.Lerp(this.transform.position.y, this.transform.position.y + 5f, Time.deltaTime), this.transform.position.z);
+            if(this.transform.position.y >= 0) 
+            {
+                _reseting = false;
+            }
+        }
+    }
+    public static void StopEnviromentAndReset() 
+    {
+        _reseting = true;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+enum RotateType 
+{
+    Right , Left
+}
+public class Player : MonoBehaviour
+{
+    Transform _playerTransform;
+    int _rotateDur = 200;
+    void Start()
+    {
+        _playerTransform = this.transform;
+    }
+
+    void Update()
+    {
+#if UNITY_ANDROID 
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.position.x > (Screen.width / 2)) 
+            {
+                Rotate(RotateType.Left);
+
+            }
+            if(touch.position.x <= (Screen.width / 2)) 
+            {
+                Rotate(RotateType.Right);
+            }
+        }
+
+#endif
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0))
+        {
+            if(Input.mousePosition.x> (Screen.width / 2)) 
+            {
+                Rotate(RotateType.Left);
+            }
+            if (Input.mousePosition.x <= (Screen.width / 2)) 
+            {
+                Rotate(RotateType.Right);
+            }
+        }
+#endif
+        }
+
+    private void Rotate(RotateType rotateType) 
+    {
+        if(rotateType == RotateType.Right)
+            _playerTransform.eulerAngles = new Vector3(_playerTransform.eulerAngles.x
+                        , _playerTransform.eulerAngles.y
+                        ,Mathf.Lerp(_playerTransform.eulerAngles.z, _playerTransform.eulerAngles.z + _rotateDur,Time.deltaTime));
+        if(rotateType == RotateType.Left)
+            _playerTransform.eulerAngles = new Vector3(_playerTransform.eulerAngles.x
+                       , _playerTransform.eulerAngles.y
+                       , Mathf.Lerp(_playerTransform.eulerAngles.z, _playerTransform.eulerAngles.z - _rotateDur, Time.deltaTime));
+    }
+}
